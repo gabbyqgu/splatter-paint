e6cc7003664515f41f72d04f1c0dca2396fccfdd
gabbyqgu <gabbyq.gu@gmail.com>
2026-05-26 18:15:32 -0700

    set up cursor art

---
+++ script.js
@@ -0,0 +1,15 @@
+tool.fixedDistance = Math.round(Math.random() * 25)
+function onMouseMove(event) {
+    //wherever the cursor goes, a circle is created
+    var path = new Path.Circle({ 
+        center: event.middlePoint,
+        radius: Math.round(Math.random() * 25) + 5 //random # btwn 5 + 30
+    })
+    path.fillColor = {
+        /* hue: 360 deg color wheel! 0 deg = red */
+        hue: event.count * 3, //color changes by 3deg every frame
+        /*hue: Math.round(Math.random() * 360), */
+        saturation: 0.6,
+        brightness: 1
+    }
+} 
